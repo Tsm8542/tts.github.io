af60ce6df246c3ddc6a138f0ac9b04d25c3c7b45
Tsm8542 <shaikhabid932@gmail.com>
2023-01-05 01:41:30 +0530

    05-01-2023 01:41 AM

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 let speech = new SpeechSynthesisUtterance();
-speech.pitch = 1;
 speech.lang = 'en';
-speech.volume = 1;
-speech.rate = 1;
-let voices = [];
-voices = speechSynthesis.getVoices();
-speech.voice = voices[0];
 
-function x(){
+let voices = [];
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+    let voiceSelect = document.querySelector("#voice");
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+};
+document.querySelector("#voice").addEventListener("change", () => {
+    speech.voice = voices[document.querySelector("#voice").value];
+});
+
+document.querySelector("#sp").addEventListener("click", () => {
     let s = document.querySelector("#tt").value;
+    speech.pitch = 1;
+    speech.volume = 1;
+    speech.rate = 1;
     speech.text = s;
     speechSynthesis.speak(speech);
-}
-function p(){
+});
+
+document.querySelector("#pa").addEventListener("click", () => {
     speechSynthesis.pause();
-}
-function r(){
+});
+
+document.querySelector("#re").addEventListener("click", () => {
     speechSynthesis.resume();
-}
-function c(){
+});
+
+document.querySelector("#ca").addEventListener("click", () => {
     speechSynthesis.cancel();
-}
+});
