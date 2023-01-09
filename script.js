d0bcd9dadeb5a555fff8a2dd941fcfd8d3c449a5
Tsm8542 <shaikhabid932@gmail.com>
2023-01-09 10:00:46 +0530

    09-01-2023 10:00 AM

--- script.js
+++ script.js
@@ -1,34 +1,17 @@
-let speech = new SpeechSynthesisUtterance();
-speech.lang = 'en';
-
-let voices = [];
-window.speechSynthesis.onvoiceschanged = () => {
-    voices = window.speechSynthesis.getVoices();
-    speech.voice = voices[0];
-    let voiceSelect = document.querySelector("#voice");
-    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
-};
-document.querySelector("#voice").addEventListener("change", () => {
+function vo(){
     speech.voice = voices[document.querySelector("#voice").value];
-});
-
-document.querySelector("#sp").addEventListener("click", () => {
+}
+function sp(){
     let s = document.querySelector("#tt").value;
-    speech.pitch = 1;
-    speech.volume = 1;
-    speech.rate = 1;
     speech.text = s;
     speechSynthesis.speak(speech);
-});
-
-document.querySelector("#pa").addEventListener("click", () => {
+}
+function pa(){
     speechSynthesis.pause();
-});
-
-document.querySelector("#re").addEventListener("click", () => {
+}
+function re(){
     speechSynthesis.resume();
-});
-
-document.querySelector("#ca").addEventListener("click", () => {
+}
+function ca(){
     speechSynthesis.cancel();
-});
+}
